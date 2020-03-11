55aef10a929bd8b0d4026358006e837bec894ca3
Maxim Batourine <m.batourine@gmail.com>
2020-03-11 16:48:55 -0400

    Second iteration of ActionsManager

--- ActionsManager.cs
+++ ActionsManager.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,91 +7,136 @@ using Crestron.SimplSharp;
 
 namespace Daniels.Common
 {
-    public static class ActionsManager
+    public class ActionInfo
     {
-        private class ActionStub
+        public readonly uint Id;
+        public readonly string Name;
+        public readonly string Help;
+        public readonly string Parameters;
+        public readonly bool ReadOnly;
+        public ActionInfo(uint id, string name, string help, bool readOnly, string parameters)
         {
-            public ActionFunction Function;
-            public string Help;
-            public Dictionary<string, string> Parameters;
-            public ActionStub(ActionFunction function, string help, Dictionary<string, string> parameters)
-            {
-                Function = function;
-                Help = help;
-                Parameters = parameters;
-            }
+            Id = id;
+            Name = name;
+            Help = help;
+            ReadOnly = readOnly;
+            Parameters = parameters;
         }
-        private static Dictionary<string, ActionStub> _actions = new Dictionary<string, ActionStub>();
+    }
+
+    public class ActionsManager: IEnumerable<ActionInfo>
+    {
+        private static ActionsManager _instance;
+
+        private static uint _id = 0;
+        private static Dictionary<uint, ActionInfo> _actionInfos = new Dictionary<uint, ActionInfo>();
+        private static Dictionary<uint, ActionFunction> _actionFunctions = new Dictionary<uint, ActionFunction>();
 
         public delegate void ActionFunction(string actionParameters);
 
-        public static bool RegisterAction(ActionFunction actionFunction, string actionName, string help)
+        private ActionsManager()
         {
-            return RegisterAction(actionFunction, actionName, help, new Dictionary<string, string>());
+
         }
 
-        public static bool RegisterAction(ActionFunction actionFunction, string actionName, string help, string paramName, string paramValue)
+        #region Static Methods
+
+        public static ActionsManager GetInstance()
         {
-            if (String.IsNullOrEmpty(paramName))
-                throw new ArgumentNullException("paramName");
-            if (String.IsNullOrEmpty(paramValue))
-                throw new ArgumentNullException("paramValue");
-            return RegisterAction(actionFunction, actionName, help, new Dictionary<string, string>() { { paramName, paramValue } });
+            if (_instance == null)
+                _instance = new ActionsManager();
+            return _instance;
         }
 
-        public static bool RegisterAction(ActionFunction actionFunction, string actionName, string help, Dictionary<string, string> parameters)
+        public static uint RegisterAction(ActionFunction function, string name, string help)
         {
-            if(_actions.ContainsKey(actionName))
-                return false;
-            if(actionFunction == null)
-                return false;
+            if(function == null)
+                throw new ArgumentNullException("function");
+            if (_actionFunctions.ContainsValue(function))
+                throw new ArgumentOutOfRangeException("function", "Action already registered");
+            if (name == null)
+                throw new ArgumentNullException("name");
+            if (String.IsNullOrEmpty(name))
+                throw new ArgumentOutOfRangeException("name");
+            if (_actionInfos.Values.FirstOrDefault(a => a.Name == name) != null)
+                throw new ArgumentOutOfRangeException("name", "Action with name " + name + " already registered");
 
-            Dictionary<string, string> actionParameters;
+            uint actionId = ++_id;
+            string actionHelp = (help == null) ? String.Empty : help;
 
-            if (parameters == null)
-                actionParameters = new Dictionary<string, string>();
-            else
-                actionParameters = parameters;
-            _actions.Add(actionName, new ActionStub(actionFunction, help, parameters));
-            //_actionsReadOnly = new ReadOnlyDictionary<string, ActionFunction>(_actions);
-            return true;
+            _actionFunctions.Add(actionId, function);
+            _actionInfos.Add(actionId, new ActionInfo(actionId, name, actionHelp, true, String.Empty));
+
+            return actionId;
         }
 
-        public static string[] GetActions()
+        public static uint AddCutomAction(uint id, string actionName, string parameters, string help)
         {
-            return _actions.Keys.ToArray();
+            if (!_actionFunctions.ContainsKey(id))
+                throw new ArgumentOutOfRangeException("id");
+            if (actionName == null)
+                throw new ArgumentNullException("name");
+            if (String.IsNullOrEmpty(actionName))
+                throw new ArgumentOutOfRangeException("name");
+            if (_actionInfos.Values.FirstOrDefault(a => a.Name == actionName) != null)
+                throw new ArgumentOutOfRangeException("name", "Action with name " + actionName + " already registered");
+            if (String.IsNullOrEmpty(parameters))
+                throw new ArgumentNullException("parameters");
+
+            string actionHelp = (help == null) ? String.Empty : help;
+            
+            uint actionId = ++_id;
+
+            _actionInfos.Add(id, new ActionInfo(actionId, actionName, actionHelp, false, parameters));
+
+            return actionId;
         }
 
-        public static string[] GetActionsParameters(string actionName)
+        public static void InvokeAction(uint id)
         {
-            if (!_actions.ContainsKey(actionName))
-                throw new ArgumentOutOfRangeException("actionName", "Action not found");
-
-            return _actions[actionName].Parameters.Keys.ToArray();
+            var kv = _actionInfos.FirstOrDefault(x => x.Value.Id == id);
+            if (kv.IsNull())
+                throw new ArgumentOutOfRangeException("id");
+            _actionFunctions[kv.Key].Invoke(kv.Value.Parameters);
         }
 
-        public static void InvokeAction(string actionName)
-        {
-            InvokeAction(actionName, null);
-        }
+        #endregion Static Methods
 
-        public static void InvokeAction(string actionName, string actionParameters)
-        {
-            if (!_actions.ContainsKey(actionName))
-                throw new ArgumentOutOfRangeException("actionName", "Action not found");
+        #region Properties
 
-            if (String.IsNullOrEmpty(actionParameters))
+        public ActionInfo this[uint id]
+        {
+            get
             {
-                _actions[actionName].Function(String.Empty);
-            }
-            else
-            {
-                if (_actions[actionName].Parameters.ContainsKey(actionParameters))
-                    _actions[actionName].Function(_actions[actionName].Parameters[actionParameters]);
+                if (_actionInfos.ContainsKey(id))
+                    return _actionInfos[id];
                 else
-                    throw new ArgumentOutOfRangeException("actionParameters", "No specified parameters found for given action");
+                    throw new IndexOutOfRangeException();
             }
         }
+
+        public uint Length
+        {
+            get { return (uint)_actionInfos.Count; }
+        }
+
+        #endregion Properties
+
+        #region IEnumerable<ActionInfo> Implementation
+
+        public IEnumerator<ActionInfo> GetEnumerator()
+        {
+            return _actionInfos.Values.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        #endregion IEnumerable<ActionInfo> Implementation
+
+
 
         /*
         private static ReadOnlyDictionary<string, ActionFunction> _actionsReadOnly = new ReadOnlyDictionary<string, ActionFunction>(_actions);
---
+++ KeyValuePairExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Crestron.SimplSharp;
+
+namespace Daniels.Common
+{
+    public static class KeyValuePairExtensions
+    {
+        public static bool IsNull<T, TU>(this KeyValuePair<T, TU> pair)
+        {
+            return pair.Equals(new KeyValuePair<T, TU>());
+        }
+    }
+}
